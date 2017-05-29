c70619dfc69d2e247f93c93dbf3b51ca28ecad70
Ricky995 <ristovskistefan95@gmail.com>
2017-05-30 00:08:18 +0200

    Finished useful link

--- netmag/netmag/settings.py
+++ netmag/netmag/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'netmag.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['/home/stefan/git_tutorial/work/hello/netmag/netmag/templates',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = ('/home/stefan/git_turotial/work/hello/netmag/netmag/static/',)
--- netmag/netmag/urls.py
+++ netmag/netmag/urls.py
@@ -20,5 +20,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
 url(r'^admin/', include(admin.site.urls)),
+url(r'^$', 'blog.views.index'),
+url(r'^(?P<slug>[\w\-]+)/$', 'blog.views.post'),
 )
 
