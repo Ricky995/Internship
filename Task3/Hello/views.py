7b2457b179510e2b1f98a54421e442a03934b265
Ricky995 <ristovskistefan95@gmail.com>
2017-05-31 21:03:20 +0200

    Uploading task3

--- Task3/Hello/views.py
+++ Task3/Hello/views.py
@@ -3,8 +3,9 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
-
+import datetime
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    date=datetime.datetime.now()
+    return HttpResponse("The time is %s" % date)
 # Create your views here.
